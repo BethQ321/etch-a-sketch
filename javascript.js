4241c8660d0ffb4067a4aa50b0a5b8e412fd61c0
bethq321 <bethqualls@comcast.net>
2024-10-02 15:35:23 -0500

    created consistent sizing of grid

--- javascript.js
+++ javascript.js
@@ -1,31 +1,32 @@
 let length;
+const grid = document.querySelector("#container");
 
 const setLengthButton = document.querySelector("#popUp");
 setLengthButton.addEventListener("click", () => {
     length = parseInt(prompt("To set the length of the grid, choose a number between 1 and 100.", ""));
-    if(typeof length != "number" || length <=0 || length > 100) {
-        choice = prompt("Not a valid input. Pick a number between 1 and 100.", "");
+    grid.innerHTML = "";
+    while(typeof length != "number" || length <=0 || length > 100) {
+        length = parseInt(prompt("Not a valid input. Pick a number between 1 and 100.", ""));
     }
     createGrid(length);
-    console.log(length);
 });
 
-const grid = document.querySelector("#container");
+
 
 function createGrid(inputLength) {
     for(let i = 0; i < inputLength; i++) {
-        console.log(inputLength);
         const gridColumn = document.createElement("div");
         gridColumn.classList.add("gridColumn");
         for(let j = 0; j < inputLength; j++) {
             const gridRow = document.createElement("span");
             gridRow.classList.add("gridRow");
+            const spanSize = 960/inputLength;
+            gridRow.style.height = `${spanSize}px`;
+            gridRow.style.width = `${spanSize}px`;
             let opacity = 1;
             gridRow.addEventListener("mouseover", () => {
                 opacity = opacity - .1;
                 gridRow.style.backgroundColor = `rgba(54, 117, 136, ${opacity})`
-                // gridRow.style.backgroundColor = "teal";
-                // gridRow.style.opacity = opacity - .1;
             })
             gridColumn.appendChild(gridRow);
         };
